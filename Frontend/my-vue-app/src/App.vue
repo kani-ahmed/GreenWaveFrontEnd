6c57ee4a86ded3e77c82cb55d3341aa2b02505a0
esteban1324 <estebanlinarez@ymail.com>
2024-03-23 15:04:41 -0400

    Co-authored-by: Pranav Dhinakar <pxd222@case.edu> Co-authored-by: kylekaufman <kylekaufman@users.noreply.github.com>

--- Frontend/my-vue-app/src/App.vue
+++ Frontend/my-vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App" />
-</template>lsnalsk
+</template> 
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
--- Frontend/my-vue-app/src/components/HelloWorld.vue
+++ Frontend/my-vue-app/src/components/HelloWorld.vue
@@ -1,58 +1,73 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <!-- html formatting for feature 1 page -->
+  <div class="impact-calculator">
+    <header>
+      <h1>Impact Calculator</h1>
+    </header>
+    <div class="actions">
+      <button @click="addBottle"> Add Bottle </button>
+      <div class="score" @click="generateImpactScore">
+        Click To Generate Your Impact Score {{ username }}!
+      </div>
+      <div class="savings" @click="getSavings">
+        Click To Get Your Savings {{ username }}!
+      </div>
+    </div>
   </div>
 </template>
 
+
+<style scoped>
+/* css formatting for feature 1 page */
+.impact-calculator header {
+  text-align: center;
+}
+
+.actions {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.actions>* {
+  margin: 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.score {
+  background-color: limegreen;
+}
+
+.savings {
+  background-color: lightgreen;
+  transform: rotate(45deg);
+}
+</style>
+
+
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'ImpactCalculator',
+  data() {
+    return {
+      username: 'USERNAME',
+      bottlesAdded: 0,
+    };
+  },
+  methods: {
+    addBottle() {
+      this.bottlesAdded++;
+      // Further logic to handle bottle addition
+    },
+    generateImpactScore() {
+      // Logic to generate impact score
+    },
+    getSavings() {
+      // Logic to calculate savings
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
