da8f33cbb9c3a9bbce6b375749ece0bbd2ab9432
kenny-ahmedd <kenny.ahmedd@gmail.com>
2024-04-12 16:29:17 -0400

    corrected server.js

--- Frontend/my-vue-app/server.js
+++ Frontend/my-vue-app/server.js
@@ -1,19 +1,15 @@
-import express from 'express';
-import serveStatic from 'serve-static';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
 
-// Get the directory path
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 // Serve the built files from the "dist" directory
-app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.use('/', serveStatic(path.join(__dirname, 'dist')));
 
-// Serve the index.html file for all routes
+// Serve the index.html file for all routes - to handle SPA routing
 app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
