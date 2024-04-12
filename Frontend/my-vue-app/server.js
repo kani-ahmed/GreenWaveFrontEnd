25085b83db44b613f9b8ab981739c020f6f0280a
kenny-ahmedd <kenny.ahmedd@gmail.com>
2024-04-12 15:40:30 -0400

    added deployment yaml files. page serving dep installed

---
+++ Frontend/my-vue-app/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import serveStatic from 'serve-static';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+// Get the directory path
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Serve the built files from the "dist" directory
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+
+// Serve the index.html file for all routes
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
