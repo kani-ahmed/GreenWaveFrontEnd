415fa0bee725635788c688e48ad6ab20dd216378
kylekaufman <ktk33@case.edu>
2024-04-19 14:19:05 -0400

    Documentation for Register page

--- Frontend/my-vue-app/src/components/RegisterPage.vue
+++ Frontend/my-vue-app/src/components/RegisterPage.vue
@@ -1,10 +1,12 @@
 <template>
+  <!-- Vue template for the register page -->
   <div class="RegisterPage">
     <header>
       <h1>Green Wave</h1>
     </header>
 
     <div class="register-form">
+      <!-- Form for user registration -->
       <form @submit.prevent="register">
         <div class="form-group">
           <label for="username">Username:</label>
@@ -29,6 +31,7 @@ import axios from 'axios';
 
 export default {
   data() {
+    // Data properties for storing user input
     return {
       username: '',
       email: '',
@@ -37,6 +40,7 @@ export default {
   },
   methods: {
     register() {
+      // Method to handle user registration
       const apiUrl = 'https://heroku-project-backend-staging-ffb8722f57d5.herokuapp.com/register';
       const userData = {
         username: this.username,
@@ -44,12 +48,16 @@ export default {
         password: this.password
       };
 
+       // Making a POST request to register the user
       axios.post(apiUrl, userData)
           .then(response => {
+            // Handling successful registration
             if (response.status === 201) {
               console.log("Registration successful!");
+              // Redirecting user to the calculator page upon successful registration
               this.$router.push('/calculator');
             } else {
+              // Handling registration error
               console.error('Registration failed:', response.data);
               alert('Registration failed');
             }
@@ -64,6 +72,7 @@ export default {
 </script>
 
 <style scoped>
+/* Styling for the register page */
 header {
   position: relative;
   text-align: center;
@@ -73,6 +82,7 @@ header {
   margin: 0;
 }
 
+/* Styling for the header background image */
 header::after {
   content: '';
   position: absolute;
@@ -85,6 +95,7 @@ header::after {
   border-radius: 8px 8px 0 0;
 }
 
+/* Styling for the register page container */
 .RegisterPage {
   font-family: 'Roboto', sans-serif;
   color: #333;
@@ -96,6 +107,7 @@ header::after {
   margin: 2rem auto;
 }
 
+/* Styling for the register form */
 .register-form {
   background: white;
   padding: 20px;
@@ -105,20 +117,24 @@ header::after {
   width: 300px;
 }
 
+/* Styling for form groups */
 .form-group {
   margin-bottom: 15px;
 }
 
+/* Styling for form labels */
 .form-group label {
   display: block;
 }
 
+/* Styling for form inputs */
 .form-group input {
   width: 100%;
   padding: 10px;
   box-sizing: border-box;
 }
 
+/* Styling for the register button */
 button {
   width: 100%;
   padding: 10px;
@@ -128,6 +144,7 @@ button {
   cursor: pointer;
 }
 
+/* Styling for hover effect on the register button */
 button:hover {
   opacity: 0.8;
 }
